1f8672649d125c51c4eca000e7bb74980577e7b2
Lucas Rezac <lucas.m.rezac@gmail.com>
2015-01-25 16:03:47 -0600

    commit 3
    Finished assigning tile ids to existing tile sprites
    Finished Map.class loader

--- src/tile/Tile.java
+++ src/tile/Tile.java
@@ -4,8 +4,16 @@ import java.awt.image.*;
 import main.Sprite;
 
 public class Tile extends Sprite{
+	private boolean isSolid;
 	public Tile(int x, int y, BufferedImage image) {
 		super(x, y, image);
+		isSolid = true;
+	}
+	public void setIsCollidable(boolean b){
+		isSolid = b;
+	}
+	public boolean isCollidable(){
+		return isSolid;
 	}
 	public void update(){}
 	//only animated tiles can be powered
